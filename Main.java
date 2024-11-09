9ee203de010d1ea7e2c920da07b903a56284db96
Jagadees27 <jagadeeswaran2705@gmail.com>
2024-11-09 22:01:32 +0530

    initial files

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Project project = new Project("Project 1");
+        System.out.println(project.name);
+    }
+}
---
+++ Project.java
@@ -0,0 +1,8 @@
+public class Project {
+    String name;
+
+    Project(String name) {
+        this.name = name;
+        System.out.println("Project created: " + name);
+    }
+}
